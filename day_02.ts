0ef09845b1b49e279175f475225efdd024c72bce
HackedServer <hs@hackedserver.net>
2020-12-13 18:55:37 -0800

    Day 02 Part 1 Done

--- day_02.ts
+++ day_02.ts
@@ -7,6 +7,25 @@ let count_total: number = 0;
 for (let line of input_file) {
     let [count, letter, password]: string[] = line.replace(/:/g, '').split(' ');
 
+    let [position_1, position_2]: number[] = count.split('-').map(Number);
+
+    let current_password: string[] = password.split('');
+
+    if ( 
+        (current_password[position_1 - 1] === letter && current_password[position_2 - 1] !== letter)
+        ||
+        (current_password[position_2 - 1] !== letter && current_password[position_2 - 1] === letter)
+        ) { 
+            count_total++;
+            console.log("We got a match");
+            console.log("Count", count, "Letter", letter, "Password", password);
+        }
+    }
+/** This is Part 1. Going to very ugly like just duplicate this until I learn functions or whatever.
+
+for (let line of input_file) {
+    let [count, letter, password]: string[] = line.replace(/:/g, '').split(' ');
+
     let [count_low, count_high]: number[] = count.split('-').map(Number);
 
     let count_actual: number = 0;
@@ -21,13 +40,6 @@ for (let line of input_file) {
         count_total++
     }
 }
-
+*/
 
 console.log("Number of valid passwords: ", count_total);
-
-
-
-/**
-6-7 z: dqzzzjbzz
-13-16 j: jjjvjmjjkjjjjjjj
-*/
